fd95715343fc213cb720e43b45c38e5832a914c5
Alessandro <ales.greenhouse@gmail.com>
2014-05-04 11:40:53 +0200

    Aggiunta cartella con il primo esercizio in prolog

---
+++ Prolog/es1/1-iterative_deepening.pl
@@ -0,0 +1,64 @@
+num_col(10).
+num_righe(10).
+
+occupata(pos(2,5)).
+occupata(pos(3,5)).
+occupata(pos(4,5)).
+occupata(pos(5,5)).
+occupata(pos(6,5)).
+occupata(pos(7,5)).
+occupata(pos(7,1)).
+occupata(pos(7,2)).
+occupata(pos(7,3)).
+occupata(pos(7,4)).
+occupata(pos(5,7)).
+occupata(pos(6,7)).
+occupata(pos(7,7)).
+occupata(pos(8,7)).
+occupata(pos(4,7)).
+occupata(pos(4,8)).
+occupata(pos(4,9)).
+occupata(pos(4,10)).
+
+iniziale(pos(4,2)).
+
+finale(pos(7,9)).
+
+
+applicabile(est,pos(R,C)) :- 
+	num_col(NC), C<NC,
+	C1 is C+1,
+	\+ occupata(pos(R,C1)).
+
+applicabile(sud,pos(R,C)) :- 
+	num_righe(NR), R<NR,
+	R1 is R+1,
+	\+ occupata(pos(R1,C)).
+
+applicabile(ovest,pos(R,C)) :- 
+	C>1,
+	C1 is C-1,
+	\+ occupata(pos(R,C1)).
+
+applicabile(nord,pos(R,C)) :- 
+	R>1,
+	R1 is R-1,
+	\+ occupata(pos(R1,C)).
+
+trasforma(est,pos(R,C),pos(R,C1)) :- C1 is C+1.
+trasforma(ovest,pos(R,C),pos(R,C1)) :- C1 is C-1.
+trasforma(sud,pos(R,C),pos(R1,C)) :- R1 is R+1.
+trasforma(nord,pos(R,C),pos(R1,C)) :- R1 is R-1.
+
+ric_prof_cc(S,_,[],_,_) :- finale(S),!.
+ric_prof_cc(S,Visitati,[Az|Resto],Depth,Limit) :-
+     Depth < Limit,
+     Depth1 is Depth+1,
+     applicabile(Az,S),
+     trasforma(Az,S,Nuovo_S),
+     \+ member(Nuovo_S,Visitati),
+     ric_prof_cc(Nuovo_S,[S|Visitati],Resto,Depth1,Limit).
+     
+iterative_deepening(S,Visitati,[Az|Resto],Limit) :-  ric_prof_cc(S,Visitati,[Az|Resto],1,Limit).
+iterative_deepening(S,Visitati,[Az|Resto],Limit) :-  NewLimit is Limit+1, write_ln(NewLimit), iterative_deepening(S,Visitati,[Az|Resto],NewLimit).
+
